e27aefa56970ac6557b68527ce54f7010ca6db42
Aashish-panwar <119643468+Aashish-panwar@users.noreply.github.com>
2026-07-25 08:10:37 +0530

    Remove CrossOrigin annotation from ConsumerController
    Removed CrossOrigin annotation to restrict CORS policy.

--- src/main/java/com/electricity/electricity_billing_backend/controller/ConsumerController.java
+++ src/main/java/com/electricity/electricity_billing_backend/controller/ConsumerController.java
@@ -18,7 +18,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/consumers")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 @Tag(
         name = "Consumer Management",
         description = "APIs for managing electricity consumers"
